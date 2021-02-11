50aeb49a8641cc9a9c2e2e072d9c876c539dc6a9
jjhiggz <jonathan.higger@gmail.com>
2021-02-11 12:31:02 -0700

    second commit

--- app.js
+++ app.js
@@ -9,10 +9,17 @@
 // element.innerTEXT = " string representing html "
 // <opening> if you have <p></p> it gets rendered as text instead of as a p tag</opening>
 
-//  directly change an attribute of a element in js
+// directly change an attribute of a element in js
 // example
 // img.src = "some other url"
 // element.classList.add("card")
 
-const address =
-	"https://scontent.fapa1-2.fna.fbcdn.net/v/t1.0-9/136944502_10224603887191818_6204706414676021234_o.jpg?_nc_cat=107&ccb=3&_nc_sid=8bfeb9&_nc_ohc=rPboJ1en24EAX97qnbT&_nc_ht=scontent.fapa1-2.fna&oh=6f11074314528ab4179dc65f8a3f0e3c&oe=6049E000";
+const $form = document.querySelector("#instrument-form");
+
+$form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	// do stuff
+	const formData = new FormData($form);
+	console.log(formData.get("instrument"), formData.get("student-name"));
+	$form.reset();
+});
